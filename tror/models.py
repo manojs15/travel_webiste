c9a744d0993bf9438a8b331b6a6711b63a2e7278
manojs15 <manojshivappa.153@gmail.com>
2024-02-19 20:16:54 +0530

    phone_number_initialize

--- tror/models.py
+++ tror/models.py
@@ -7,6 +7,7 @@ class TravelBooking(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
     places_to_visit = models.TextField()
+    phone_number = models.BigIntegerField()  # Storing phone number as a complete integer
 
     def __str__(self):
         return f"Booking for {self.total_people} people - {self.vehicle_type}"
